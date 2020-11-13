5706e310c985a42c7d2175570b14c8fdda959fa4
Sam <sjb578@msstate.edu>
2020-11-12 21:53:00 -0600

    sequenceNumLog working

--- client.cpp
+++ client.cpp
@@ -217,7 +217,7 @@ int main(int argc, char *argv[]) {
                     lengthPacket = fileLength % 30;
                 }
 
-
+                sequenceNumberLog << packetSequenceNum; << endl;
 
 
                 // CHECK IF ITS THE LAST PACKET
